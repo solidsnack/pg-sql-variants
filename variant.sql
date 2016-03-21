249a6a8687a760f70a1abffd1c541e721fcdaa70
Jason Dusek <jason.dusek@gmail.com>
2016-03-20 22:05:05 -0700

    Implement view

--- variant.sql
+++ variant.sql
@@ -9,7 +9,7 @@ SET LOCAL search_path TO variants, meta, public;
 --- to have the same names.)
 CREATE FUNCTION variant(base regclass, variant regclass) RETURNS void AS $code$
 DECLARE
-  view_name      text;
+  view_name      text := quote_ident(tablename(base)||'*');
   trigger_base   text := tablename(base)||':'||tablename(variant);
   insert_trigger text := quote_ident(trigger_base||'/i');
   update_trigger text := quote_ident(trigger_base||'/u');
@@ -17,9 +17,22 @@ DECLARE
   ns             text := quote_ident(schemaname(variant));
   selects        text[];
 BEGIN
-  --- Setup the foreign key linking variant to base.
+  --- Collect table information now, before changing the search path, to use
+  --- for rebuilding the view, later.
+  WITH info AS (SELECT pk.* FROM fk JOIN pk USING (tab) WHERE other = base
+                 UNION
+                SELECT * FROM pk WHERE tab = variant)
+  SELECT array_agg($$
+    SELECT $$|| quote_cols(cols) ||$$,
+           tableoid::regclass,
+           row_to_json((tab))
+      FROM $$|| tab ||$$ AS tab
+  $$) INTO STRICT selects FROM info;
+
   EXECUTE $$
     SET LOCAL search_path TO $$|| ns ||$$, public;
+
+  ----- Setup the foreign key linking variant to base.
 
     ALTER TABLE $$|| variant ||$$ ADD FOREIGN KEY ($$||
       quote_cols(pk(variant))
@@ -32,7 +45,7 @@ BEGIN
       quote_cols(pk(variant))
     ||$$ FROM $$|| variant ||$$;
 
-    --- Create the triggers that propagate changes to base.
+  ----- Create the triggers that propagate changes to base.
 
     CREATE OR REPLACE FUNCTION $$|| insert_trigger ||$$()
     RETURNS trigger AS $t$
@@ -67,20 +80,15 @@ BEGIN
     CREATE TRIGGER $$|| delete_trigger ||$$
     BEFORE DELETE ON $$|| variant ||$$ FOR EACH ROW
     EXECUTE PROCEDURE $$|| delete_trigger ||$$();
+
+  ----- Rebuild the view.
+
+    SET LOCAL search_path TO $$|| ns ||$$, public;
+    CREATE OR REPLACE VIEW $$|| view_name ||$$ ($$||
+      quote_cols(pk(base))
+    ||$$, type, data) AS$$||
+    array_to_string(selects, '   UNION ALL')||$$
   $$;
---   --- Rebuild the view.
---   SELECT array_agg($$
---     SELECT $$|| quote_cols(pk.cols) ||$$,
---            tableoid::regclass,
---            row_to_json((tab))
---       FROM $$|| tab ||$$ AS tab
---   $$) INTO STRICT selects FROM fk JOIN pk USING (tab) WHERE other = base;
---   EXECUTE $$
---     SET LOCAL search_path TO $$|| ns ||$$, public;
---     CREATE VIEW $$|| view_name ||$$ ($$||
---       quote_cols(pk(base))
---     ||$$, type, data) AS$$||
---     array_to_string(selects, '   UNION');
 END
 $code$ LANGUAGE plpgsql SET search_path FROM CURRENT;
 
